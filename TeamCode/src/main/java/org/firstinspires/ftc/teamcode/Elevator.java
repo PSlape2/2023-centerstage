fed928859414efaaf00a090de14d0c93a367ca45
Sanjib Rath <srath@qti.qualcomm.com>
2023-10-12 20:15:01 -0700

    more stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
@@ -1,2 +1,37 @@
-package org.firstinspires.ftc.teamcode;public class Elevator {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Elevator extends LinearOpMode {
+    private DcMotor motor;
+    private final float MAX_VALUE = 1000;
+    private final float MIN_VALUE = 0;
+    public Elevator() {
+        motor = hardwareMap.get(DcMotor.class, "Elevator Motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void runOpMode(){
+       //Set gamepad 2 joystick on the controller to move the elevator up and down
+
+        boolean update = false;
+
+        while (opModeIsActive()) {
+            float updated = -gamepad2.left_stick_y;
+            float position = motor.getCurrentPosition();
+
+            if (position <= MIN_VALUE) {
+                if (updated > 0) motor.setPower(updated);
+            } else if(position >= MAX_VALUE) {
+                if (updated < 0) motor.setPower(updated);
+            }
+
+
+        }
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
@@ -1,2 +1,40 @@
-package org.firstinspires.ftc.teamcode;public class Grabber {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class Grabber extends LinearOpMode {
+    private final float MAX_VALUE = 1000;
+
+    private final float MIN_VALUE = 0;
+    Servo one;
+    Servo two;
+
+    private boolean state;
+    public Grabber() {
+        one = hardwareMap.get(Servo.class, "Servo 1");
+        two = hardwareMap.get(Servo.class, "Servo 2");
+        state = false;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        if(gamepad2.a) {
+
+            if(!state) {
+                one.setPosition(MAX_VALUE); // Set the servo position to the maximum
+                two.setPosition(MIN_VALUE); // Set the servo position to the maximum
+                state = true;
+            }
+        } else {
+            two.setPosition(MAX_VALUE); // Set the servo position to the maximum
+            one.setPosition(MIN_VALUE); // Set the servo position to the default
+            state = false;
+
+
+        }
+
+    }
 }
