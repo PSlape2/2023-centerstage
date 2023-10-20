e1d80afd398e7dc5ca4ac09f2c3663e3b0432755
Max Backer <max.r.backer@gmail.com>
2023-10-19 20:07:24 -0700

    meow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
@@ -6,16 +6,16 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Grabber extends LinearOpMode {
-    private final float MAX_VALUE = 1000;
+    private final float MAX_VALUE = 1000;// initializes float, Max Value/Power = 1000
 
-    private final float MIN_VALUE = 0;
+    private final float MIN_VALUE = 0;//initializes float, Minimum Value/Power = 0
     Servo one;
     Servo two;
 
     private boolean state;
-    public Grabber() {
-        one = hardwareMap.get(Servo.class, "Servo 1");
-        two = hardwareMap.get(Servo.class, "Servo 2");
+    public Grabber() { //gets two servos, Servo 1 and Servo 2.
+        one = hardwareMap.get(Servo.class, "Servo 1");//gets Servo class #1 from hardwareMap
+        two = hardwareMap.get(Servo.class, "Servo 2");//gets Servo class #2 from hardwareMap
         state = false;
     }
 
