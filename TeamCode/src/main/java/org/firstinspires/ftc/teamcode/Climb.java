eb11e3926b63fb22f4ffb41220e2151cd172d5c1
Jason Ballinger <jason.ballinger06@gmail.com>
2023-10-26 20:24:04 -0700

    climb is fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Climb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Climb.java
@@ -6,32 +6,44 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class Climb extends LinearOpMode {
+    private DcMotor DcMotor1;
+    private DcMotor DcMotor2;
 
-    private DcMotor DcMotor1 = null;
-    private DcMotor DcMotor2 = null;
-
+    // TODO: Change the extended constant to the proper value
     private static final int EXTENDED = 1000;
+    // TODO: Change the retracted constant to the proper value
     private static final int RETRACTED = 0;
 
     @Override
     public void runOpMode() {
         DcMotor1 = hardwareMap.get(DcMotor.class, "DcMotor1" );
         DcMotor2 = hardwareMap.get(DcMotor.class, "DcMotor2" );
-        DcMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        DcMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         double constPower = 1; //TODO: What is "use final"? Or what is a good value for constant power?
 
         boolean last = false;
 
+        waitForStart();
+
         while (opModeIsActive()) {
-            if(gamepad2.dpad_up) {
+            if (gamepad2.dpad_up) {
                 DcMotor1.setTargetPosition(EXTENDED);
-                DcMotor2.setTargetPosition(-EXTENDED);
-            } else if(gamepad2.dpad_down) {
+                DcMotor2.setTargetPosition(EXTENDED);
+            } else if (gamepad2.dpad_down) {
                 DcMotor1.setTargetPosition(RETRACTED);
-                DcMotor2.setTargetPosition(-RETRACTED);
+                DcMotor2.setTargetPosition(RETRACTED);
             }
+
+            if (gamepad2.dpad_up || gamepad2.dpad_down) {
+                DcMotor1.setPower(0.3);
+                DcMotor2.setPower(0.3);
+            } else {
+                DcMotor1.setPower(0);
+                DcMotor2.setPower(0);
+            }
+
+            DcMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            DcMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
     }
 }
