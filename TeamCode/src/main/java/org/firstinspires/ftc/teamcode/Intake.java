1501cb2d2546e47d1e4f55af47d01b2f1a56de9b
Breas <=>
2023-11-09 21:00:33 -0800

    more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,2 +1,19 @@
-package org.firstinspires.ftc.teamcode;public class Intake {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake {
+    private Servo servo1 = null;
+
+    public Intake( Servo servo1) {
+
+        this.servo1 = servo1;
+
+    }
+    public void setServo(double pos) {
+        servo1.setPosition(pos);
+    }
+
+
+
 }
