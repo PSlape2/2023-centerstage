7b7c1e401662e0e6ba85e8e1082df2ff7fa67705
Max Backer <max.r.backer@gmail.com>
2023-11-14 18:25:58 -0800

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -4,11 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter {
-
     private Servo servo1;
-    @Override
-    public void runOpMode() {
-        servo1 = hardwareMap.get(Servo.class, "shooter");
 
     public Shooter(Servo servo1) {
         this.servo1 = servo1;
