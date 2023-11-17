5f00d6645d737a3546098bcbae9e20038568f162
Peyton S <pey.slape@gmail.com>
2023-11-16 18:43:27 -0800

    direv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAutoOpMode.java
@@ -1,2 +1,25 @@
-package org.firstinspires.ftc.teamcode;public class DriveAutoOpMode {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+@Autonomous(name="Move to Middle Line", group="Robot", preselectTeleOp="MainJavaOpMode")
+public class DriveAutoOpMode extends LinearOpMode {
+    private static final double kDriveSpeed = 0.6;
+    private static final double kTurnSpeed = 0.5;
+    private Drivetrain drive;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new Drivetrain(
+                hardwareMap.dcMotor.get("frontLeftMotor"),
+                hardwareMap.dcMotor.get("backLeftMotor"),
+                hardwareMap.dcMotor.get("frontRightMotor"),
+                hardwareMap.dcMotor.get("backRightMotor"),
+                hardwareMap.get(IMU.class, "imu")
+        );
+
+        waitForStart();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -5,11 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.checkerframework.checker.units.qual.C;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 // FIELD CENTRIC
 public class Drivetrain {
+    private static final double COUNTS_PER_INCH = 6;
     private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
     private IMU imu;
     public Drivetrain(DcMotor frontLeftMotorIn, DcMotor backLeftMotorIn, DcMotor frontRightMotorIn, DcMotor backRightMotorIn, IMU imuIn) {
@@ -21,6 +24,8 @@ public class Drivetrain {
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
 
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
@@ -35,6 +40,11 @@ public class Drivetrain {
         double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
         double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         rotX *= 1.1; // Counteract imperfect strafing
 
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
@@ -48,8 +58,44 @@ public class Drivetrain {
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
     }
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeOut) {
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        ElapsedTime runtime = new ElapsedTime();
+        int leftTarget, rightTarget;
+
+        leftTarget = frontLeftMotor.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+        rightTarget = frontRightMotor.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+        frontRightMotor.setTargetPosition(rightTarget);
+        backRightMotor.setTargetPosition(rightTarget);
+        frontLeftMotor.setTargetPosition(leftTarget);
+        backLeftMotor.setTargetPosition(leftTarget);
+
+        frontRightMotor.setPower(speed);
+        backRightMotor.setPower(speed);
+        frontLeftMotor.setPower(speed);
+        backLeftMotor.setPower(speed);
+
+        while((runtime.seconds() < timeOut) && (frontLeftMotor.isBusy() || frontRightMotor.isBusy())) {}
+
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+    }
 
     public void imuResetYaw() {
         imu.resetYaw();
+    }
+
+    public double getRightPosition() {
+        return frontRightMotor.getCurrentPosition();
+    }
+    public double getLeftPosition() {
+        return frontLeftMotor.getCurrentPosition();
     }
 }
