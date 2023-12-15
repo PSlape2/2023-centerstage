a885271ffd713388da99587e8988170e193f3d12
Max Backer <max.r.backer@gmail.com>
2023-12-14 19:27:22 -0800

    place middle auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlaceAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlaceAutoOpMode.java
@@ -8,10 +8,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="Place on Middle Line", group="Robot", preselectTeleOp="MainJavaOpMode")
 public class PlaceAutoOpMode extends LinearOpMode {
-    private static final double kDriveSpeed = 0.6;
-    private static final double kTurnSpeed = 0.5;
-    private static final double kElevatorExtensionSpeed = 0.3;
-    private static final double kElevatorAngleSpeed = 0.3;
+    private static final double SpeedDrive = 0.6;
+    private static final double SpeedTurn = 0.5;
+    private static final double ElevatorExtensionSpeed = 0.3;
+    private static final double ElevatorAngleSpeed = 0.3;
 
     private Drivetrain drive;
     private Elevator elevator;
@@ -30,50 +30,25 @@ public class PlaceAutoOpMode extends LinearOpMode {
                 hardwareMap.get(DcMotor.class, "Angle Motor")
         );
         grabber = new Grabber(
-                hardwareMap.get(Servo.class, "GrabberServo"),
-                hardwareMap.get(Servo.class, "PusherServo")
+                hardwareMap.get(Servo.class, "GrabberServo")
         );
-
-
         waitForStart();
 
         grabber.setPusher(Grabber.MIN_PUSHER_POSITION);
 
         sleep(250);
 
-                // leftInches and rightInches are not in inches
-        drive.encoderDrive(0.25, 1000, 1000, 0.7);
-        //drive.encoderDrive(kTurnSpeed, -1000, 1000, 3);
-
-        telemetry.addData("Status: ", "Finished Drive");
-        telemetry.update();
-
-        sleep(250);
-
-                // about 1000 counts per revolution, 2.66 revolutions to max (probably)
-        elevator.setAutoAngle(1000);
-        elevator.setAutoExtend(1000);
-
-        telemetry.addData("Status: ", "Extended");
-        telemetry.update();
+        drive.timeDrive(SpeedDrive, 0.5);
 
         sleep(250);
 
         grabber.setPusher(Grabber.MAX_PUSHER_POSITION);
-        grabber.setIntake(Grabber.MIN_INTAKE_POSITION);
 
-        telemetry.addData("Status: ", "Pixel Placed");
-        telemetry.update();
+        sleep(500);
 
-        sleep(250);
-
-        elevator.setAutoExtend(500);
-        elevator.setAutoAngle(500);
-        grabber.setIntake(Grabber.MAX_INTAKE_POSITION);
-
-        telemetry.addData("Status: ", "Auto Finished");
-        telemetry.update();
-
-        sleep(250);
     }
 }
+
+
+// set auto angle and set auto extend
+// sleep()
